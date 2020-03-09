fea685dbdae9adaf4721b30fe22838bd7802a1fc
hp <hp@DESKTOP-U78EJ0R>
2020-03-09 17:38:33 +0530

    logging

---
+++ logging-niha/src/main/java/week5/House_ConstructionTest.java
@@ -0,0 +1,18 @@
+package week5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import week5.House_Construction;
+
+public class House_ConstructionTest {
+
+	@Test
+	public void testcost() {
+		House_Construction h=new House_Construction();
+		assertEquals(12500.00 ,h.cost("high", "yes", 5),0);
+				
+	}
+
+}
